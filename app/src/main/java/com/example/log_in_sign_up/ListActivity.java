573d8c3243a441418d16aaa46c8b69377cf6f157
Nicko Lander Rosales Milan <135952476+nickolandermilan30@users.noreply.github.com>
2024-03-15 18:26:07 +0800

    March 15 2024

--- app/src/main/java/com/example/log_in_sign_up/ListActivity.java
+++ app/src/main/java/com/example/log_in_sign_up/ListActivity.java
@@ -2,8 +2,12 @@ package com.example.log_in_sign_up;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +16,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ListActivity extends AppCompatActivity {
+
 
     private static final String PREFS_NAME = "MyPrefsFile";
     private static final String ITEM_SET_KEY = "itemSet";
@@ -33,10 +38,8 @@ public class ListActivity extends AppCompatActivity {
         // Initialize adapter with empty list
         adapter = new ArrayAdapter<>(this, R.layout.list_item_layout, new ArrayList<String>());
 
-        // Set adapter to list view
-        listView.setAdapter(adapter);
 
-        // Add new item if available
+        // Add new item
         String userName = getIntent().getStringExtra("userName");
         String hotelName = getIntent().getStringExtra("hotelName");
         String location = getIntent().getStringExtra("location");
@@ -44,23 +47,50 @@ public class ListActivity extends AppCompatActivity {
         String roomName = getIntent().getStringExtra("roomName");
         String selectedDate = getIntent().getStringExtra("selectedDate");
 
-        if (userName != null && hotelName != null && location != null && activityName != null && roomName != null && selectedDate != null) {
-            String itemDetails = userName + "\n" +
-                    hotelName + "\n" +
-                    location + "\n" +
-                    activityName + "\n" +
-                    roomName + "\n" +
-                    selectedDate;
+        String itemDetails = userName + "\n" +
+                 hotelName + "\n" +
+                 location + "\n" +
+                activityName + "\n" +
+                roomName + "\n" +
+                selectedDate;
 
-            // Add the item details to itemSet
-            itemSet.add(itemDetails);
+        itemSet.add(itemDetails);
 
-            // Save items
-            saveItems();
+        // Save items
+        saveItems();
 
-            // Update adapter with new item
-            adapter.add(itemDetails);
-        }
+        // Set adapter
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item_layout, new ArrayList<String>(itemSet)) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = convertView;
+                if (view == null) {
+                    LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                    view = inflater.inflate(R.layout.list_item_layout, null);
+                }
+
+                TextView userNameTextView = view.findViewById(R.id.userNameTextView);
+                TextView hotelNameTextView = view.findViewById(R.id.hotelNameTextView);
+                TextView locationTextView = view.findViewById(R.id.locationTextView);
+                TextView activityNameTextView = view.findViewById(R.id.activityNameTextView);
+                TextView roomNameTextView = view.findViewById(R.id.roomNameTextView);
+                TextView selectedDateTextView = view.findViewById(R.id.selectedDateTextView);
+
+                // Split itemDetails to get individual fields
+                String[] fields = getItem(position).split("\n");
+
+                // Set text for TextViews
+                userNameTextView.setText(fields[0]);
+                hotelNameTextView.setText(fields[1]);
+                locationTextView.setText(fields[2]);
+                activityNameTextView.setText(fields[3]);
+                roomNameTextView.setText(fields[4]);
+                selectedDateTextView.setText(fields[5]);
+
+                return view;
+            }
+        };
+        listView.setAdapter(adapter);
     }
 
     private void loadItems() {
